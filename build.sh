7d41953e395c91f5906ca61be50de7e6b6a9174c
Alexander Gudulin <alexandr.gudulin@gmail.com>
2012-03-03 23:48:52 +0400

    Changed success mesasge

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 cd ./src
-bison -d pyparser.y --verbose && flex lexer.l && g++ -c lex.yy.c pyparser.tab.c && g++ -o parser lex.yy.o pyparser.tab.o -ll -ly && echo "OK ;)"
+bison -d pyparser.y --verbose && flex lexer.l && g++ -c lex.yy.c pyparser.tab.c && g++ -o parser lex.yy.o pyparser.tab.o -ll -ly && echo ">> OK"
 rm lex.yy.* pyparser.output pyparser.tab.*
